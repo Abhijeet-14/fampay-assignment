4426e297d725b266200f5a36088dc77c326e40ea
abhijeet-14 <abhijeet.rts@gmail.com>
2022-10-07 03:32:07 +0530

    add YT data API details in constants

---
+++ fampay_assignment/app_api/constants.py
@@ -0,0 +1,9 @@
+SCHEDULE_JOB_IN_SEC = 10
+BASE_URL = "https://content-youtube.googleapis.com/youtube/v3/search"
+
+SEARCH_TYPE = "video"
+SEARCH_PART = "snippet"
+
+SEARCH_MAX_RESULT = 25
+
+API_KEY = "API_KEY"
