ad1c13f4b80cf0c7d900b39edd3b6a81c6c54e5f
abhijeet-14 <abhijeet.rts@gmail.com>
2022-10-07 04:42:07 +0530

    search/ add

--- fampay_assignment/app_api/urls.py
+++ fampay_assignment/app_api/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     # Author
     path("youtubevideos/", views.YouTubeVideoData.as_view(), name="video-meta-api"),
+    path("search/", views.SearchVideo.as_view(), name="video-meta-api"),
 ]
