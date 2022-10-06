81af599a7fd5b74bad62089d30ed74655ba227b6
abhijeet-14 <abhijeet.rts@gmail.com>
2022-10-07 04:41:24 +0530

    add search controller

--- fampay_assignment/app_api/video_meta/controller.py
+++ fampay_assignment/app_api/video_meta/controller.py
@@ -33,7 +33,25 @@ def get_youtube_video_list_controller(request):
         return formattedException(err, {"isTrue": False}, func_name)
 
 
+def search_video_by_title_or_description_controller(request):
+    """add video's meta data to DB"""
+    func_name = get_func_name(inspect.currentframe())
+    try:
+        logger.info(f"Enter : {func_name}")
 
+        result = search_video_by_title_or_description(request)
+
+        logger.info(f"Exit : {func_name}")
+
+        return Response({
+                "result": result,
+                "isTrue": True
+            }, 
+            status=status.HTTP_200_OK
+        )
+    except Exception as err:
+        return formattedException(err, {"isTrue": False}, func_name)
+        
 def add_youtube_video_controller(request):
     """add video's meta data to DB"""
     func_name = get_func_name(inspect.currentframe())
