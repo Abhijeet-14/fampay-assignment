fa23a6d83063f04736b27953ff95b6cddd640cc4
abhijeet-14 <abhijeet.rts@gmail.com>
2022-10-07 04:16:21 +0530

    add search api on title and description

--- fampay_assignment/app_api/views.py
+++ fampay_assignment/app_api/views.py
@@ -1,11 +1,12 @@
 import inspect
+from re import search
 
 from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
 
 from common.common_function import custom_logger
-from app_api.video_meta.controller import add_youtube_video_controller, get_youtube_video_list_controller
+from app_api.video_meta.controller import add_youtube_video_controller, get_youtube_video_list_controller, search_video_by_title_or_description_controller
 
 logger = custom_logger(__name__)
 
@@ -16,3 +17,7 @@ class YouTubeVideoData(APIView):
 
     def post(self, request):
         return add_youtube_video_controller(request)
+
+class SearchVideo(APIView):
+    def get(self, request):
+        return search_video_by_title_or_description_controller(request)
