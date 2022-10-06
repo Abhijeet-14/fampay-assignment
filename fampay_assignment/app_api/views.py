c79d1ef3d5a9a7155fa894dc8f692bdc174e5fd3
abhijeet-14 <abhijeet.rts@gmail.com>
2022-10-06 18:22:17 +0530

    add logger to API

--- fampay_assignment/app_api/views.py
+++ fampay_assignment/app_api/views.py
@@ -1,10 +1,16 @@
-from django.shortcuts import render
+import inspect
 
 from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
 
+from common.common_function import custom_logger, get_func_name
+logger = custom_logger(__name__)
+
 # Create your views here.
 class Test(APIView):
     def get(self, request):
+        func_name = get_func_name(inspect.currentframe())
+
+        logger.info(f"Entering {func_name}: GET call for Test API")
         return Response({"result": "Test_API"}, status=status.HTTP_200_OK)
