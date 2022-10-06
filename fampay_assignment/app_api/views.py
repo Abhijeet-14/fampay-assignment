46fcea801e2e97332e1b1b1677537ec2b9d31581
abhijeet-14 <abhijeet.rts@gmail.com>
2022-10-07 03:31:23 +0530

    add GET & POST method

--- fampay_assignment/app_api/views.py
+++ fampay_assignment/app_api/views.py
@@ -4,20 +4,15 @@ from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
 
-from common.common_function import custom_logger, get_func_name
-from exceptions.CustomExceptions import formattedException
+from common.common_function import custom_logger
+from app_api.video_meta.controller import add_youtube_video_controller, get_youtube_video_list_controller
 
 logger = custom_logger(__name__)
 
 # Create your views here.
-class Test(APIView):
+class YouTubeVideoData(APIView):
     def get(self, request):
-        func_name = get_func_name(inspect.currentframe())
-        try:
-            logger.info(f"Entering {func_name}: GET call for Test API")
-            
-            div = 1/0
+        return get_youtube_video_list_controller(request)
 
-            return Response({"result": "Test_API"}, status=status.HTTP_200_OK)
-        except Exception as err:
-            return formattedException(err, {"isTrue": False}, func_name)
+    def post(self, request):
+        return add_youtube_video_controller(request)
